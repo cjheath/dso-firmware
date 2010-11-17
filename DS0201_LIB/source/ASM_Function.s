a14e68cf7c57652c53e14dc91ca7ecfdcaf7c98b
Tormod Volden <debian.tormod@gmail.com>
2010-11-17 22:11:21 +0100

    LIB: make ASM_Function.s more gcc compatible

--- DS0201_LIB/source/ASM_Function.s
+++ DS0201_LIB/source/ASM_Function.s
@@ -3,6 +3,11 @@
 ; File description: start addresses for exported functions
 ;*******************************************************************************
 
+#ifdef GCC
+#define EXPORT .globl
+#define EXTERN .globl
+#endif
+
   EXTERN USB_Istr
   EXTERN CTR_HP
   EXTERN MSD_WriteBlock
@@ -13,28 +18,39 @@
   
   EXPORT __APP_Start  
   
+#ifdef GCC
+.cpu cortex-m3
+.syntax unified
+.thumb
+.text
+#else
   RSEG CODE:CODE(2)
+#endif
   
 ;*******************************************************************************
 ; Table of library function start addresses
 ;*******************************************************************************
 __APP_Start                ;void __APP_Start(void)
     B       0x0800C121
-__USB_Istr
+__USB_Istr                 ;
     B       USB_Istr
-__CTR_HP
+__CTR_HP                   ;
     B       CTR_HP
-__MSD_WriteBlock
+__MSD_WriteBlock           ;
     B       MSD_WriteBlock
-__MSD_ReadBlock
+__MSD_ReadBlock            ;
     B       MSD_ReadBlock
-__Get_Font_8x14
+__Get_Font_8x14            ;
     B       Get_Font_8x14
-__Get_Ref_Wave
+__Get_Ref_Wave             ;
     B       Get_Ref_Wave
 __SD_Set_Changed           ;void SD_Set_Changed(void);
     B       SD_Set_Changed
+#ifdef GCC
+  .end
+#else
   END
+#endif
 
 ;******************************* END OF FILE ***********************************
   
